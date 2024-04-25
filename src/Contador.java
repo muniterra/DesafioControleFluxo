9432c716f485a3fd755038825053acbd8db45896
muniterra <shutt.oficial@gmail.com>
2024-04-25 06:54:05 -0300

    Programa criado e finalizado

---
+++ src/Contador.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Contador {
+	
+	public static void main(String[] args) {
+		
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		
+		try {
+			
+			contar(parametroUm, parametroDois);
+		
+		} catch (ParametrosInvalidosException exception) {
+			
+			System.out.println(exception.getMessage());
+			
+		}
+		
+	}
+	
+	
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) throw new ParametrosInvalidosException();
+		
+		int contagem = parametroDois - parametroUm;
+		for(int i = 0; i < contagem; i++) {
+			System.out.printf("Imprimindo o número %d%n", i+1);
+		}
+	}
+	
+	
+}
